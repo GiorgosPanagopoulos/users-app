c2ba00c477b7d81e0774c39f740a23bd341a1c07
G. Panagopoulos <panagopoulosgevrgios@gmail.com>
2025-03-28 18:49:15 +0200

    update

--- usersApp/server.js
+++ usersApp/server.js
@@ -6,10 +6,11 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(
     () => {
       console.log("Connection to MongoDB established");
+
+      app.listen(port, ()=>{
+        console.log("Server is up")
+      })
     },
     err => { console.log('Failed to connect to MongoDB', err); }
   )
 
-app.listen(port, ()=>{
-  console.log("Server is up")
-})
