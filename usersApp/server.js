52da3f75fad3d65bf6c80046879684afa5422ea5
G. Panagopoulos <panagopoulosgevrgios@gmail.com>
2025-03-28 18:32:49 +0200

    update

--- usersApp/server.js
+++ usersApp/server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const app = require('./app');
 const port = 3000;
 
-mongoose.connect("")
+mongoose.connect(process.env.MONGODB_URI)
   .then(
     () => {
       console.log("Connection to MongoDB established");
